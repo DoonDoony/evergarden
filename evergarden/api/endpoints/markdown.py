e4cf24ebcbc4032f2f61f12db05dcff7300dbc06
DoonDoony <cream.doondoon@gmail.com>
2021-05-20 01:29:56 +0900

    :sparkles: Add an API upload a file to AWS S3

--- evergarden/api/endpoints/markdown.py
+++ evergarden/api/endpoints/markdown.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
 
@@ -12,3 +12,8 @@ async def html(request: Request) -> str:
     content = await request.body()
     markdown_text = content.decode("UTF-8")
     return markdown_service.to_html(markdown_text)
+
+
+@router.post("/assets")
+async def assets(file: UploadFile = File(...)) -> str:
+    return await markdown_service.upload_to_s3(file)
--- evergarden/services/markdown.py
+++ evergarden/services/markdown.py
@@ -1,14 +1,25 @@
+import uuid
+from pathlib import Path
+
 import markdown
+from fastapi import UploadFile
 
 from evergarden.core.config import settings
+from evergarden.utils.aws.s3 import S3
 
 
 class MarkdownService:
 
+    s3 = S3(settings.AWS_S3_ASSET_BUCKET_NAME)
     md = markdown.Markdown(extensions=settings.MARKDOWN_EXTENSIONS)
 
     def to_html(self, text: str) -> str:
         return self.md.convert(text)
+
+    async def upload_to_s3(self, file: UploadFile) -> str:
+        extensions = Path(file.filename).suffix
+        key = uuid.uuid4().hex + extensions
+        return await self.s3.upload(key, file)
 
 
 markdown_service = MarkdownService()
