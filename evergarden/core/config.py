b30a5372258d5726f073b395d3aa61fba426b08c
DoonDoony <cream.doondoon@gmail.com>
2021-05-19 22:53:12 +0900

    :wrench: Add python-markdown extensions

--- evergarden/core/config.py
+++ evergarden/core/config.py
@@ -1,8 +1,27 @@
+from typing import List
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     API_URL_PREFIX = "/api"
+    MARKDOWN_EXTENSIONS: List[str] = [
+        "pymdownx.betterem",
+        "pymdownx.caret",
+        "pymdownx.details",
+        "pymdownx.emoji",
+        "pymdownx.inlinehilite",
+        "pymdownx.keys",
+        "pymdownx.magiclink",
+        "pymdownx.mark",
+        "pymdownx.progressbar",
+        "pymdownx.smartsymbols",
+        "pymdownx.striphtml",
+        "pymdownx.superfences",
+        "pymdownx.tabbed",
+        "pymdownx.tasklist",
+        "pymdownx.tilde",
+    ]
 
 
 settings = Settings()
