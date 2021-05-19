089bb7dcd86284239d3f644dc21a6de73c95deb7
DoonDoony <cream.doondoon@gmail.com>
2021-05-19 22:54:28 +0900

    :sparkles: Update API to response text/html with real rendered HTML

--- evergarden/api/endpoints/markdown.py
+++ evergarden/api/endpoints/markdown.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
 from starlette.requests import Request
+
+from evergarden.services.markdown import markdown_service
 
 router = APIRouter()
 
 
-@router.post("/html")
-async def html(request: Request) -> bytes:
-    return await request.body()
+@router.post("/html", response_class=HTMLResponse)
+async def html(request: Request) -> str:
+    content = await request.body()
+    markdown_text = content.decode("UTF-8")
+    return markdown_service.to_html(markdown_text)
